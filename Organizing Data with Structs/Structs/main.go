0f4b3b6a6f806523d81438fa542059c7d243bd36
Mohsin <mohsink9899@gmail.com>
2021-08-17 19:54:49 +0530

    Section Completed

--- Organizing Data with Structs/Structs/main.go
+++ Organizing Data with Structs/Structs/main.go
@@ -39,10 +39,22 @@ func main() {
 	narissa.contactInfo.email = "narissaf@gmail.com"
 	narissa.contactInfo.zipCode = 45128
 
+	var anotherPerson person
+	anotherPerson.firstName = "Jeremy"
+	anotherPerson.lastName = "Zucker"
+	anotherPerson.contactInfo.email = "jeremyZucker@gmail.com"
+	anotherPerson.contactInfo.zipCode = 74154
+
+	//One way of printing and updating the values in structure
 	fmt.Println(narissa)
 	narissaPointer := &narissa
 	narissaPointer.updateName("Jenny")
 	narissa.print()
+
+	//Another way of printing and updating the values in structure
+	fmt.Println(anotherPerson)
+	anotherPerson.updateName("Jerem")
+	anotherPerson.print()
 }
 
 //Creating Reciever functions for structure
@@ -52,5 +64,5 @@ func (pointerToPerson *person) updateName(newFirstName string) {
 }
 
 func (p person) print() {
-	fmt.Printf("%+v ", p)
+	fmt.Printf("%+v \n", p)
 }
