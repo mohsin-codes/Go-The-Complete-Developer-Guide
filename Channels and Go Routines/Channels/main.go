58f3b4406d75793b1b10859f39ec9823aa8318ee
Mohsin <mohsink9899@gmail.com>
2021-08-18 19:26:56 +0530

    Added channel for Go Routine

--- Channels and Go Routines/Channels/main.go
+++ Channels and Go Routines/Channels/main.go
@@ -8,23 +8,31 @@ import (
 func main() {
 	links := []string{
 		"http://google.com",
+		"http://amazon.com",
 		"http://facebook.com",
 		"http://stackoverflow.com",
 		"http://golang.org",
-		"http://amazon.com",
 	}
+
+	c := make(chan string)
 
 	for _, link := range links {
 		//Adding a go routine
-		go checkLink(link)
+		go checkLink(link, c)
+	}
+
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- "It might be down!"
 	}
 
 	fmt.Println(link, "is up!")
+	c <- "Yep it's up!"
 }
