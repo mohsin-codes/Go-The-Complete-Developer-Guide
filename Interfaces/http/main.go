ddc44fb9add00308deb89c41687641ea5f917d98
Mohsin <mohsink9899@gmail.com>
2021-08-18 17:00:04 +0530

    Added a Custom Type to implement write method of Writer Interface. Also, End of Interface Section.

--- Interfaces/http/main.go
+++ Interfaces/http/main.go
@@ -7,6 +7,8 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	resp, err := http.Get("http://google.com")
 	if err != nil {
@@ -19,6 +21,16 @@ func main() {
 	// resp.Body.Read(bs)
 	// fmt.Println(string(bs))
 
+	//Implenting Custom Writer Interface that logs out elements from the body
+	lw := logWriter{}
+
 	//using write method to print the elements of the body
-	io.Copy(os.Stdout, resp.Body)
+	io.Copy(lw, resp.Body)
+}
+
+//Definition of Write function as custom implementation of Writer Interface
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Length of processed bytes is : ", len(bs))
+	return len(bs), nil
 }
